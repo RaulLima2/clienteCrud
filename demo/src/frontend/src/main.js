f839966467afe46cad89a86cc0508498323edce7
raullima2 <raulfotografialima@gmail.com>
2022-02-27 19:59:11 -0300

    fix(must): remove path frontend in src path

--- demo/src/frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp.config.productionTip = false
-new createApp({
-    router,
-    render: h => h(App)
-}).mount('#app')
